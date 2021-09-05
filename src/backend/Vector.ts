713c8ff62d404a84cc3314cd5134d202891d7e01
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-09-05 23:23:05 +0700

    Bump Deps, IncVersion.js and Multiply Function

--- src/backend/Vector.ts
+++ src/backend/Vector.ts
@@ -1,21 +1,31 @@
 // * Vector Class: Heart of the Website
 
 export class Vector {
-    readonly i: number;
-    readonly j: number;
-    readonly k: number;
+    private _i: number;
+    private _j: number;
+    private _k: number;
 
-    readonly norm: number;
-    readonly size: number;
+    private _norm: number;
+    private _size: number;
 
     // * Construct Vector with null coalescing
     constructor(i: number, j: number, k: number) {
-        this.i = i ?? 0;
-        this.j = j ?? 0;
-        this.k = k ?? 0;
+        this._i = i ?? 0;
+        this._j = j ?? 0;
+        this._k = k ?? 0;
 
-        this.norm = this.i * this.i + this.j * this.j + this.k * this.k;
-        this.size = Math.sqrt(this.norm);
+        this.calcSize();
+    }
+
+    get i(): number { return this._i; }
+    get j(): number { return this._j; }
+    get k(): number { return this._k };
+    get norm(): number { return this._norm; }
+    get size(): number { return this._size; }
+
+    private calcSize(): void {
+        this._norm = this.i * this.i + this.j * this.j + this.k * this.k;
+        this._size = Math.sqrt(this.norm);
     }
 
     mult(scalar: number): Vector {
@@ -47,5 +57,11 @@ export class Vector {
 
     parallelogramArea(other: Vector): number {
         return this.crossProd(other).size;
+    }
+
+    multSelf(scalar: number): void {
+        this._i *= scalar;
+        this._j *= scalar;
+        this._k *= scalar;
     }
 }
